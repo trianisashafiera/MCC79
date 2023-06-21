1cf49030667577ccaf42dd1f31756c0a52d1fd85
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-21 15:47:56 +0700

    Update University.cs

--- API/Models/University.cs
+++ API/Models/University.cs
@@ -4,22 +4,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace API.Models
 {
     [Table ("tb_m_universities")]
-    public class University
+    public class University : BaseEntity
     {
-        [Key]
-        [Column ("guid")]
-        public Guid Guid { get; set; }
 
         [Column ("code", TypeName = "nvarchar(50)")]
         public string Code { get; set; }
 
         [Column("name", TypeName = "nvarchar(100)")]
         public string Name { get; set; }
-        //tipe data selain string tidak perlu ditulis
-        [Column("created_date")]
-        public DateTime CreatedDate { get; set; }
-        [Column("modified_date")]
-        public DateTime ModifiedDate { get; set; }
-
+        
+     
+        public ICollection<Education> Educations { get; set; }
     }
 }
