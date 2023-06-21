8255c0d582c8e57d225d74f577e6e4efae0401c2
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-21 15:48:54 +0700

    Update Room.cs

--- API/Models/Room.cs
+++ API/Models/Room.cs
@@ -4,21 +4,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace API.Models
 {
     [Table("tb_m_rooms")]
-    public class Room
+    public class Room : BaseEntity
     {
-        [Key]
-        [Column("guid")]
-        public Guid Guid { get; set; }
-
         [Column("name", TypeName = "nvarchar(50)")]
         public string Name { get; set; }
         [Column ("floor")]
         public int Floor { get; set; }
         [Column ("capacity")]
         public int Capacity { get; set; }
-        [Column("created_date")]
-        public DateTime CreatedDate { get; set; }
-        [Column("modified_date")]
-        public DateTime ModifiedDate { get; set; }
+      
     }
 }
