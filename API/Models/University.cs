1f2516e1ee7008e65c1f7e7104f967d9e575b906
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-22 09:03:18 +0700

    Update University.cs

--- API/Models/University.cs
+++ API/Models/University.cs
@@ -6,6 +6,7 @@ namespace API.Models
     [Table ("tb_m_universities")]
     public class University : BaseEntity
     {
+       
 
         [Column ("code", TypeName = "nvarchar(50)")]
         public string Code { get; set; }
@@ -13,7 +14,7 @@ namespace API.Models
         [Column("name", TypeName = "nvarchar(100)")]
         public string Name { get; set; }
         
-     
+        //Cardinality
         public ICollection<Education> Educations { get; set; }
     }
 }
