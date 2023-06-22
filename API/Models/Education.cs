15859d2119028ad7d539f858fd2312458df0f711
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-22 09:03:33 +0700

    Create Education.cs

---
+++ API/Models/Education.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    [Table("tb_m_educations")]
+    public class Education : BaseEntity
+    {
+      
+        [Column("major", TypeName = "nvarchar(100)")]
+        public string Major { get; set; }
+
+        [Column("degree", TypeName = "nvarchar(100)")]
+        public string Degree { get; set; }
+
+        [Column("gpa")]
+        public float GPA { get; set; }
+
+        [Column("university_id")]
+        public Guid UniversityGuid { get; set; }
+
+        // Cardinality
+        public University University { get; set; }
+        public Employee Employee { get; set; }  
+
+    }
+}
