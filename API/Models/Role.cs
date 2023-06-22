26d084e42a138cc35b9f700a09314b18f5fc7680
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-22 09:03:24 +0700

    Update Role.cs

--- API/Models/Role.cs
+++ API/Models/Role.cs
@@ -4,17 +4,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace API.Models
 {
     [Table ("tb_m_roles")]
-    public class Role
+    public class Role : BaseEntity
     {
-        [Key]
-        [Column ("guid")]
-        public Guid Guid { get; set; }
-
         [Column("name", TypeName = "nvarchar(100)")]
         public string Name { get; set; }
-        [Column("created_date")]
-        public DateTime CreatedDate { get; set; }
-        [Column("modified_date")]
-        public DateTime ModifiedDate { get; set; }
+
+        public ICollection<AccountRole> AccountRoles { get; set; }
+
     }
 }
