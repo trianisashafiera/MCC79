6f488bcd426a2481a7dded72715cb1f8953095fc
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-22 09:03:38 +0700

    Update AccountRole.cs

--- API/Models/AccountRole.cs
+++ API/Models/AccountRole.cs
@@ -4,21 +4,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace API.Models
 {
     [Table ("tb_tr_account_roles")]
-    public class AccountRole
+    public class AccountRole : BaseEntity
     {
-        [Key]
-        [Column("guid")]
-        public Guid Guid { get; set; }
-
         [Column("account_guid")]
         public Guid AccountGuid { get; set; }
 
         [Column("role_guid")]
         public Guid RoleGuid { get; set; }
 
-        [Column("created_date")]
-        public DateTime CreatedDate { get; set; }
-        [Column("modified_date")]
-        public DateTime ModifiedDate { get; set; }
+        public Account Account { get; set; }
+        public Role Role { get; set; }
+
     }
 }
