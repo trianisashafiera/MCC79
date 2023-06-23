7ce943ca4911677e43086c0c9289b2ce21812457
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:53:12 +0700

    Create IRoomRepository.cs

---
+++ API/Contracts/IRoomRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public interface IRoomRepository
+    {
+        ICollection<Room> GetAll();
+        Room? GetByGuid(Guid guid);
+        Room Create(Room room);
+
+        bool Update(Room room);
+        bool Delete(Room room);
+
+    }
+}
