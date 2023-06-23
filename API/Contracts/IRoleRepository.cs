ae4411d7f979a656e70fbe4690b4a7f653171552
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:53:17 +0700

    Create IRoleRepository.cs

---
+++ API/Contracts/IRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public interface IRoleRepository
+    {
+        ICollection<Role> GetAll();
+        Role? GetByGuid(Guid guid);
+        Role Create(Role role);
+        bool Update(Role role);
+        bool Delete(Guid guid);
+    }
+}
