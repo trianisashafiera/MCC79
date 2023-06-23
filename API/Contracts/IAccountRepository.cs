7b2d672c68a310e208f09803a6c7408ac3839758
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:54:24 +0700

    Create IAccountRepository.cs

---
+++ API/Contracts/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public interface IAccountRepository
+    {
+        ICollection<Account> GetAll();
+        Account? GetByGuid(Guid guid);
+        Account Create(Account account);
+        bool Update(Account account);
+        bool Delete(Guid guid);
+
+    }
+}
