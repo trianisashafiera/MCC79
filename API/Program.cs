6adf1131326f696cdf2f7adc1718b1d000879047
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:50:22 +0700

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,28 @@
+using API.Contracts;
+using API.Data;
+using API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+
+//Add DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BookingDbContext>(options => options.UseSqlServer(connectionString));
+
+//Add Repository to the container
+builder.Services.AddScoped<IUniversityRepository, UniversityRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScope<IAccountRoleRepository, AccountRoleRepository>();
+builder.Services.AddScope<IBookingRepository, BookingRepository>();
+builder.Services.AddScope<IEducationRepository, EducationRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
