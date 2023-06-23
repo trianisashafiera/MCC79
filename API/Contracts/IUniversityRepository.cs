d42c8cc7df4e026fa8f7efb0af9e5b83443bf792
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:53:09 +0700

    Create IUniversityRepository.cs

---
+++ API/Contracts/IUniversityRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public interface IUniversityRepository
+    {
+        ICollection<University> GetAll();
+        University? GetByGuid(Guid guid);
+        University Create(University university);
+        bool Update(University university);
+        bool Delete(Guid guid);
+    }
+}
