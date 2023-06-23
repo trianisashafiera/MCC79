b9bc196255968e691a963e4a1e7aed6fa8cdeb18
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:52:39 +0700

    Create UniversityController.cs

---
+++ API/Controllers/UniversityController.cs
@@ -0,0 +1,70 @@
+﻿using API.Contracts;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/universities")]
+    public class UniversityController : ControllerBase
+    {
+        private readonly IUniversityRepository _repository;
+
+        public UniversityController(IUniversityRepository repository)        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var univerities = _repository.GetAll();
+            if (!univerities.Any()) 
+            {
+                return NotFound();
+            }
+            return Ok(univerities);
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid) 
+        {
+            var university = _repository.GetByGuid(guid);
+            if (university is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(university);
+        }
+
+        [HttpPost]
+        public IActionResult Create(University university) 
+        { 
+            var createdUniversity = _repository.Create(university);
+            return Ok(createdUniversity);
+        }
+
+        [HttpPut]
+        public IActionResult Update(University university)
+        {
+            var isUpdated = _repository.Update(university);
+            if (!isUpdated)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid guid)
+        {
+            var isDeleted = _repository.Delete(guid);
+            if (!isDeleted)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+    }
+}
