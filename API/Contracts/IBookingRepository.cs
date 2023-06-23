5da9aed89274daaa62db6e92874a798e053b5ca2
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:54:17 +0700

    Create IBookingRepository.cs

---
+++ API/Contracts/IBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public interface IBookingRepository
+    {
+        ICollection<Booking> GetAll();
+        Booking? GetByGuid(Guid guid);
+        Booking Create(Booking booking);
+        bool Update(Booking booking);    
+        bool Delete(Guid guid);
+
+    }
+}
