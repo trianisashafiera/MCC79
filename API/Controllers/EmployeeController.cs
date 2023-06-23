4f625247fcc2849df038cdc568d1b0949816ffa7
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:52:52 +0700

    Create EmployeeController.cs

---
+++ API/Controllers/EmployeeController.cs
@@ -0,0 +1,69 @@
+﻿using API.Contracts;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _repository;
+        public EmployeeController(IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var employees = _repository.GetAll();
+            if (!employees.Any())
+            {
+                return NotFound();
+            }
+            return Ok(employees);
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            var employee = _repository.GetByGuid(guid);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            var createdEmployee = _repository.Create(employee);
+            return Ok(createdEmployee);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Employee employee)
+        {
+            var isUpdated = _repository.Update(employee);
+            if (!isUpdated)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid guid)
+        {
+            var isDeleted = _repository.Delete(guid);
+            if (!isDeleted)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+    }
+}
