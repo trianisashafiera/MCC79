00c7759c984559ab98132df62a560e436eac5701
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:54:12 +0700

    Create IEducationRepository.cs

---
+++ API/Contracts/IEducationRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public interface IEducationRepository
+    {
+        ICollection<Education> GetAll();
+        Education? GetByGuid(Guid guid);
+        Education Create(Education education);
+        bool Update(Education education);
+        bool Delete(Guid guid);
+
+    }
+}
