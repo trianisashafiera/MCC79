48d7cd5daaced927847557deb91530c8bbbf1039
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-23 13:54:09 +0700

    Create IEmployeeRepository.cs

---
+++ API/Contracts/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public interface IEmployeeRepository
+    {
+        ICollection<Employee> GetAll();
+        Employee? GetByGuid(Guid guid);
+        Employee Create (Employee employee);
+        bool Update(Employee employee);
+        bool Delete(Guid guid); 
+    }
+}
