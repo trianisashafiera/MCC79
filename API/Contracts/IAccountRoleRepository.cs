ea95ba221eb31d42526ba93db193dc21b4a6b765
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-25 15:56:57 +0700

    Update IAccountRoleRepository.cs

--- API/Contracts/IAccountRoleRepository.cs
+++ API/Contracts/IAccountRoleRepository.cs
@@ -1,13 +1,7 @@
 ﻿using API.Models;
 
-namespace API.Contracts
+namespace API.Contracts;
+public interface IAccountRoleRepository : IGeneralRepository<AccountRole>
 {
-    public interface IAccountRoleRepository
-    {
-        ICollection<AccountRole> GetAll();
-        AccountRole? GetByGuid(Guid guid);
-        AccountRole Create(AccountRole accountRole);
-        bool Update(AccountRole accountRole);
-        bool Delete(Guid guid);
-    }
+
 }
