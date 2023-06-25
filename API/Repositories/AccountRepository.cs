b5e83764f826a63df8209e4e31afbf6e37caeb15
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-25 15:55:36 +0700

    Update AccountRepository.cs

--- API/Repositories/AccountRepository.cs
+++ API/Repositories/AccountRepository.cs
@@ -2,71 +2,9 @@
 using API.Data;
 using API.Models;
 
-namespace API.Repositories
-{
-    public class AccountRepository : IAccountRepository
+namespace API.Repositories;
+
+    public class AccountRepository : GeneralRepository<Account>, IAccountRepository
     {
-        private readonly BookingDbContext _context;
-        public AccountRepository(BookingDbContext context)
-        {
-            _context = context;
-        }
-
-        public ICollection<Account> GetAll()
-        {
-            return _context.Set<Account>().ToList();
-        }
-
-        public Account? GetByGuid(Guid guid)
-        {
-            return _context.Set<Account>().Find(guid);
-        }
-
-        public Account Create(Account account)
-        {
-            try
-            {
-                _context.Set<Account>().Add(account);
-                _context.SaveChanges();
-                return account;
-            }
-            catch
-            {
-                return new Account();
-            }
-        }
-
-        public bool Update(Account account)
-        {
-            try
-            {
-                _context.Set<Account>.Update(account);
-                _context.SaveChanges();
-                return true;
-            }
-            catch 
-            { 
-                return false; 
-            }
-        }
-
-        public bool Delete(Guid guid)
-        {
-            try
-            {
-                var account = GetByGuid(guid);
-                if (account is null)
-                {
-                    return false;
-                }
-                _context.Set<Account>().Remove(account);
-                _context.SaveChanges();
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
+    public AccountRepository(BookingDbContext context) : base(context) { }
     }
-}
