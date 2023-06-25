1c208763bc3e6e50fc5b170ad46399b1ca8100a4
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-25 15:56:03 +0700

    Create GeneralController.cs

---
+++ API/Controllers/GeneralController.cs
@@ -0,0 +1,68 @@
+﻿using API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+    public class GeneralController<TEntity> : ControllerBase
+        where TEntity : class
+    {
+        private readonly IGeneralRepository<TEntity> _repository;
+        public GeneralController(IGeneralRepository<TEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var entity = _repository.GetAll();
+            if (!entity.Any())
+            {
+                return NotFound();
+            }
+            return Ok(entity);
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            var entity = _repository.GetByGuid(guid);
+            if (entity is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TEntity entity)
+        {
+            var createdEntity = _repository.Create(entity);
+            return Ok(createdEntity);
+        }
+
+        [HttpPut]
+        public IActionResult Update(TEntity entity)
+        {
+            var isUpdated = _repository.Update(entity);
+            if (!isUpdated)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid guid)
+        {
+            var isDeleted = _repository.Delete(guid);
+            if (!isDeleted)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+    }
+
