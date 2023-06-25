b7ca9a48ab96e6ef8597c31f7d8f311ba69c45dc
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-25 15:55:14 +0700

    Create GeneralRepository.cs

---
+++ API/Repositories/GeneralRepository.cs
@@ -0,0 +1,74 @@
+﻿using API.Contracts;
+using API.Data;
+
+namespace API.Repositories;
+
+public class GeneralRepository<TEntity> : IGeneralRepository<TEntity>
+where TEntity : class
+{
+    protected readonly BookingDbContext _context;
+
+    public GeneralRepository(BookingDbContext context)
+    {
+        _context = context;
+    }
+
+    public ICollection<TEntity> GetAll()
+    {
+        return _context.Set<TEntity>().ToList();
+    }
+
+    public TEntity? GetByGuid(Guid guid)
+    {
+        return _context.Set<TEntity>().Find(guid);
+    }
+
+    public TEntity? Create(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public bool Update(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Update(entity);
+            _context.SaveChanges();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public bool Delete(Guid guid)
+    {
+        try
+        {
+            var entity = GetByGuid(guid);
+            if (entity is null)
+            {
+                return false;
+            }
+
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+ 
