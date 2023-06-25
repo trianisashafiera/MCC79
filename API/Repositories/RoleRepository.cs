b58a2be708e9e0828d2209e1c3b7cadcc3ad0304
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-25 15:55:08 +0700

    Update RoleRepository.cs

--- API/Repositories/RoleRepository.cs
+++ API/Repositories/RoleRepository.cs
@@ -2,70 +2,8 @@
 using API.Data;
 using API.Models;
 
-namespace API.Repositories
-{
-    public class RoleRepository : IRoleRepository
+namespace API.Repositories;
+    public class RoleRepository : GeneralRepository<Role>, IRoleRepository
     {
-        private readonly BookingDbContext _context;
-        public RoleRepository(BookingDbContext context)
-        {
-            _context = context;
-        }
-        public ICollection<Role> GetAll()
-        {
-            return _context.Set<Role>().ToList();
-        }
-
-        public Role? GetByGuid(Guid guid)
-        {
-            return _context.Set<Role>().Find(role);
-        }
-
-        public Role Create(Role role)
-        {
-            try
-            {
-                _context.Set<Role>().Add(role);
-                _context.SaveChanges();
-                return role;
-            }
-            catch
-            {
-                return new Role();
-            }
-        }
-
-        public bool Update(Role role)
-        {
-            try
-            {
-                _context.Set<Role>().Update(role);
-                _context.SaveChanges();
-                return role;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public bool Delete(Guid guid)
-        {
-            try
-            {
-                var role = GetByGuid(guid);
-                if (role is null)
-                {
-                    return false;
-                }
-                _context.Set<Role>().Remove(role);
-                _context.SaveChanges();
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
+        public RoleRepository(BookingDbContext context) : base(context) { }
     }
-}
