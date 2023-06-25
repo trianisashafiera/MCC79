fa13f7d520ebe7a330763b03975dceaaba4f2512
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-25 15:56:42 +0700

    Create IGeneralRepository.cs

---
+++ API/Contracts/IGeneralRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Contracts;
+public interface IGeneralRepository<TEntity>
+{
+    ICollection<TEntity> GetAll();
+    TEntity? GetByGuid(Guid guid);
+    TEntity? Create(TEntity entity);
+    bool Update(TEntity entity);
+    bool Delete(Guid guid);
+}
+
