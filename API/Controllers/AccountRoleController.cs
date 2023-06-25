a83e280ce4ddfcfd0790f6a5abb90550520878b1
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-25 15:56:22 +0700

    Update AccountRoleController.cs

--- API/Controllers/AccountRoleController.cs
+++ API/Controllers/AccountRoleController.cs
@@ -2,69 +2,10 @@
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
-{
+namespace API.Controllers;
     [ApiController]
     [Route("api/account-roles")]
-    public class AccountRoleController : ControllerBase
-    {
-        private readonly IAccountRoleRepository _repository;
-        public AccountController(IAccountRoleRepository repository)
-        {
-            _repository = repository;
-        }
-
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var accountRoles = _repository.GetAll();
-            if (!accountRoles.Any())
-            {
-                return NotFound();
-            }
-            return Ok(accountRoles);
-        }
-
-        [HttpGet("{guid}")]
-        public IActionResult GetByGuid(Guid guid)
-        {
-            var accountRole = _repository.GetByGuid(guid);
-            if (accountRole is null)
-            {
-                return NotFound();
-            }
-
-            return Ok(accountRole);
-        }
-
-        [HttpPost]
-        public IActionResult Create(AccountRole accountRole)
-        {
-            var createdAccountRole = _repository.Create(accountRole);
-            return Ok(createdAccountRole);
-        }
-
-        [HttpPut]
-        public IActionResult Update(AccountRole accountRole)
-        {
-            var isUpdated = _repository.Update(accountRole);
-            if (!isUpdated)
-            {
-                return NotFound();
-            }
-
-            return Ok();
-        }
-
-        [HttpDelete]
-        public IActionResult Delete(Guid guid)
-        {
-            var isDeleted = _repository.Delete(guid);
-            if (!isDeleted)
-            {
-                return NotFound();
-            }
-            return Ok();
-        }
-    }
+public class AccountRoleController : GeneralController<AccountRole>
+{
+    public AccountRoleController(IAccountRoleRepository repository) : base(repository) { }
 }
