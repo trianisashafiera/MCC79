d2ad1a16a841029a0366d427b449db117c232e04
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-25 15:55:31 +0700

    Update AccountRoleRepository.cs

--- API/Repositories/AccountRoleRepository.cs
+++ API/Repositories/AccountRoleRepository.cs
@@ -2,71 +2,9 @@
 using API.Data;
 using API.Models;
 
-namespace API.Repositories
-{
-    public class AccountRoleRepository : IAccountRoleRepository
+namespace API.Repositories;
+
+    public class AccountRoleRepository : GeneralRepository<AccountRole>, IAccountRoleRepository
     {
-        private readonly BookingDbContext _context;
-        public AccountRoleRepository(BookingDbContext context)
-        {
-            _context = context;
-        }
-
-        public ICollection<AccountRole> GetAll()
-        {
-            return _context.Set<AccountRole>().ToList();
-        }
-
-        public AccountRole? GetByGuid(Guid guid)
-        {
-            return _context.Set<AccountRole>().Find(guid);
-        }
-
-        public AccountRole Create(AccountRole accountRole)
-        {
-            try
-            {
-                _context.Set<AccountRole>().Add(accountRole);
-                _context.SaveChanges();
-                return accountRole;
-            }
-            catch
-            {
-                return new AccountRole();
-            }
-        }
-
-        public bool Update(AccountRole accountRole)
-        {
-            try
-            {
-                _context.Set<AccountRole>().Update(accountRole);
-                _context.SaveChanges();
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public bool Delete(Guid guid)
-        {
-            try
-            {
-                var accountRole = GetByGuid(guid);
-                if (accountRole is null)
-                {
-                    return false;
-                }
-                _context.Set<AccountRole>().Remove(accountRole);
-                _context.SaveChanges();
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
+        public AccountRoleRepository(BookingDbContext context) : base(context) { }
     }
-}
