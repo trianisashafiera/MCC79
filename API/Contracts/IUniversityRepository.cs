bd96b68d49e3ff624b3975a333ea8f2a2c5b3976
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-25 15:56:31 +0700

    Update IUniversityRepository.cs

--- API/Contracts/IUniversityRepository.cs
+++ API/Contracts/IUniversityRepository.cs
@@ -1,13 +1,7 @@
 ﻿using API.Models;
 
-namespace API.Contracts
+namespace API.Contracts;
+public interface IUniversityRepository : IGeneralRepository<University>
 {
-    public interface IUniversityRepository
-    {
-        ICollection<University> GetAll();
-        University? GetByGuid(Guid guid);
-        University Create(University university);
-        bool Update(University university);
-        bool Delete(Guid guid);
-    }
+
 }
